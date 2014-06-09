cbe9863e4cb3bc738339a1a473bcaf61774bb61e
colprog <colprog@gmail.com>
2014-06-09 12:24:00 +0800

    fix

--- game-server/app/utils/dispatcher.js
+++ game-server/app/utils/dispatcher.js
@@ -8,25 +8,11 @@ module.exports.dispatch = function(key, list) {
 module.exports.defaultRouter = function(session, msg, app, cb) {
     var servers = app.getServersByType(msg.serverType);
     var uid = session ? (session.uid || '') : '';
-
-    if (servers.length === 1 || uid === '') {
-        cb(null, servers[0].id);
-    }
-    else {
-
-        cb(null, servers[Math.abs(crc32.calculate(uid)) % servers.length].id);
-    }
+    cb(null, servers[Math.abs(crc32.calculate(uid)) % servers.length].id);
 };
 
 module.exports.routeByPartition = function(session, msg, app, cb) {
     var servers = app.getServersByType(msg.serverType);
     var partId = session ? (session.get("partId") || '') : '';
-
-    if (servers.length === 1 || partId === '') {
-        cb(null, servers[0].id);
-    }
-    else {
-
-        cb(null, servers[Math.abs(crc32.calculate(partId)) % servers.length].id);
-    }
+    cb(null, servers[Math.abs(crc32.calculate(partId)) % servers.length].id);
 };
