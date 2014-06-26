e707b1ef41847a0d73e000805f8ed9fe14ec1638
colprog <colprog@gmail.com>
2014-06-26 22:04:57 +0800

    implement name validation

--- game-server/app/servers/game/handler/tutorialHandler.js
+++ game-server/app/servers/game/handler/tutorialHandler.js
@@ -4,17 +4,63 @@ var Constants = require("../../../../../shared/constants");
 var wrapSession = require("../../../utils/monkeyPatch").wrapSession;
 var Promise = require("bluebird");
 var _ = require("lodash");
+var fs = require("fs");
+var AhoCorasick = require('aho-corasick.js');
 var logger;
 
+var patternTrie = null;
 
 module.exports = function (app) {
     return new TutorialHandler(app);
 };
 
+function readLines(input, func, end) {
+    var remaining = '';
+
+    input.on('data', function(data) {
+        remaining += data;
+        var index = remaining.indexOf('\n');
+        while (index > -1) {
+            var line = remaining.substring(0, index);
+            remaining = remaining.substring(index + 1);
+            func(line);
+            index = remaining.indexOf('\n');
+        }
+    });
+
+    input.on('end', function() {
+        if (remaining.length > 0) {
+            func(remaining);
+        }
+        end();
+    });
+}
+
 class TutorialHandler extends base.HandlerBase {
     constructor(app) {
         this.app = app;
         logger = require('../../../utils/rethinkLogger').getLogger(app);
+
+        if (!patternTrie) {
+            patternTrie = new AhoCorasick.TrieNode();
+            var input = fs.createReadStream(app.getBase() + "/config/data/reject.txt");
+            readLines(input, function (data) {
+                data = data.trim();
+                if (data) {
+                    patternTrie.add(data);
+                }
+            }, function () {
+                AhoCorasick.add_suffix_links(patternTrie);
+            });
+        }
+    }
+
+    testNameValid(name) {
+        var valid = true;
+        AhoCorasick.search(name, patternTrie, function(word) {
+            if (word) valid = false;
+        });
+        return valid;
     }
 
     setName(msg, session, next) {
@@ -25,7 +71,7 @@ class TutorialHandler extends base.HandlerBase {
         if (role.tutorial !== 1) {
             return this.errorNext(Constants.TutorialFailed.TutorialStateError, next);
         }
-        if (!newName) {
+        if (!newName || newName.length < 2 || newName.length >= 20 || !this.testNameValid(newName)) {
             return this.errorNext(Constants.NameInvalid, next);
         }
 
