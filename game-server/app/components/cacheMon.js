c8aa8d3fb7fc60672a64070df165b38cf91a0b1a
lichuan <308831759@qq.com>
2014-10-21 20:46:08 +0800

    fix import tbl bug

--- game-server/app/components/cacheMon.js
+++ game-server/app/components/cacheMon.js
@@ -15,14 +15,14 @@ var Module = function(opts) {
 
 Module.prototype.monitorHandler = function(agent, msg, cb) {
     switch (msg.type) {
-        case "heroDef":
+        case "herodef":
             this.cacheIns.loadHeroDef();
             break;
-        case "gemDef":
-        case "itemDef":
+        case "gemdef":
+        case "itemdef":
             this.cacheIns.loadItemDef();
             break;
-        case "equipmentDef":
+        case "equipmentdef":
             this.cacheIns.loadEquipmentDef();
             break;
         case "level":
@@ -31,8 +31,8 @@ Module.prototype.monitorHandler = function(agent, msg, cb) {
         case "storeitem":
             this.cacheIns.loadStoreItem();
             break;
-        case "drawNode":
-        case "heroDraw":
+        case "drawnode":
+        case "herodraw":
             this.cacheIns.loadHeroDraws();
             break;
         case "partition":
@@ -51,16 +51,16 @@ Module.prototype.clientHandler = function(agent, msg, cb) {
     if (msg.server) serverTypes = [msg.server];
     else {
         switch (msg.type) {
-            case "heroDef":
-            case "itemDef":
-            case "equipmentDef":
-            case "gemDef":
+            case "herodef":
+            case "itemdef":
+            case "equipmentdef":
+            case "gemdef":
             case "level":
                 serverTypes = ["game", "connector"];
                 break;
             case "storeitem":
-            case "drawNode":
-            case "heroDraw":
+            case "drawnode":
+            case "herodraw":
                 serverTypes = ["game"];
                 break;
             case "partition":
